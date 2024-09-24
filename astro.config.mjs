8abb17a05b7a53e3f8910ef38fbc10aafc931ac9
Adri PG <adriprog2002@gmail.com>
2024-09-24 13:48:58 +0200

    Changing domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://adjepg.github.io',
+  site: 'https://www.adriapulido.dev',
   base: '/Portfolio/',
   integrations: [react(), tailwind()],
   i18n: {
